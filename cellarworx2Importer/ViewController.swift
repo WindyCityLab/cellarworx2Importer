6e139fcba233112aeb4a339bfccdfbbc36dbd3bd
Kevin McQuown <kevin@thewcl.com>
2016-06-04 08:07:23 -0500

    Initial Commit

---
+++ cellarworx2Importer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cellarworx2Importer
+//
+//  Created by Kevin McQuown on 6/4/16.
+//  Copyright © 2016 Windy City Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
