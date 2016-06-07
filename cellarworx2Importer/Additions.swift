d142dea6dade7de53602ddb440cce0b370092b5e
Kevin McQuown <kevin@thewcl.com>
2016-06-07 07:48:27 -0500

    added additions

---
+++ cellarworx2Importer/Additions.swift
@@ -0,0 +1,31 @@
+import Foundation
+public class Additions: PFObject, PFSubclassing
+{
+    public static func parseClassName() -> String {
+        return "Additions"
+    }
+
+    //  Strings 
+    @NSManaged public var innoculationbrand : String
+    @NSManaged public var labtest : String
+
+    //  Bools
+
+    //  Floats
+
+    //  Ints
+    @NSManaged public var bleedamount : Int
+    @NSManaged public var brix : Int
+    @NSManaged public var dapamount : Int
+    @NSManaged public var daycount : Int
+    @NSManaged public var goamount : Int
+    @NSManaged public var htaamount : Int
+    @NSManaged public var id : Int
+    @NSManaged public var innoculationamount : Int
+    @NSManaged public var superfoodamt : Int
+    @NSManaged public var wateramount : Int
+
+    //  Enums
+
+    //  Enum Getters & Setters
+}
